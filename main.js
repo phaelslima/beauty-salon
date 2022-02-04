e4a0b3c18beca0b30819d69c60a368240b888567
phaelslima <phaelslima@gmail.com>
2022-02-04 20:16:06 -0300

    feat: add about and services sections

--- main.js
+++ main.js
@@ -14,3 +14,14 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+const header = document.querySelector('#header')
+const navHeigth = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeigth) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
