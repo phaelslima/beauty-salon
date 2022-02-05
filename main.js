b96ef080b8d117f145272ec3f0030017570e27f8
phaelslima <phaelslima@gmail.com>
2022-02-05 00:17:47 -0300

    feat: add footer

--- main.js
+++ main.js
@@ -15,16 +15,16 @@ for (const link of links) {
   })
 }
 
-const header = document.querySelector('#header')
-const navHeigth = header.offsetHeight
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeigth = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeigth) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 const swiper = new Swiper('.swiper', {
   slidesPerView: 1,
@@ -47,6 +47,24 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links`,
+  #contact .text, #contact .links,
+  #footer .brand, fotter .social`,
   { internal: 100 }
 )
+
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+
+  window.addEventListener('scroll', function () {
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  })
+}
+
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+})
